7e8ab56f9299a43934385e1116d7dcd93544a732
Dovydas Majauskas <dovydasmajauskas1@gmail.com>
2025-12-15 20:08:02 +0100

    done

--- lab-react-discord-hero/src/App.jsx
+++ lab-react-discord-hero/src/App.jsx
@@ -2,34 +2,36 @@
 import './App.css';
 import logo from './assets/discord-logo-white.png'
 import menu from './assets/menu-icon.png'
-import friends from './assets/discord-background.png'
 
-function App() {
+function App(){
   return (
-    <>
     <div className="App">
-    <img className="logo" src={logo} alt="logo-white" />
-    <img className="menu" src={menu} alt="menu-white" />
-    </div> 
-    <h1>IMAGINE A 
-    <br />
-    PLACE...
-    </h1>
-    <p>...where you can belong to a school club, a gaming group, or a
-    <br />
-    worldwide art community. Where just you and a handful of
-    <br />
-    friends can spend time together. A place that makes it easy together
-    <br />
-    talk every day and hang out more often.
-    </p>
-    <button className='Mac'>Download for Mac</button>
-    <br/>
-    <button className='Open'>Open Discord in your browser</button>
-    <img className="friends" src={friends} alt="menu-white" />
-    </>
+    <header className="header">
+      <img className="logo" src={logo} alt="logo-white" />
+      <img className="menu" src={menu} alt="menu-white" />
+    </header>
 
-  );
-}
+    <section className="hero">
+       <div className="hero-text">
+          <h1>IMAGINE A PLACE…</h1>
+          <p>
+            ...where you can belong to a school club, a gaming group, or a
+            worldwide art community. Where just you and a handful of
+            friends can spend time together. A place that makes it easy together
+            talk every day and hang out more often.
+          </p>
+          <div className="buttons">
+            <button className="btn-mac">Download for Mac</button>
+            <button className="btn-open">Open Discord in your browser</button>
+          </div>
+        </div>
 
-export default App;
+        <div className="hero-image">
+          <img src="./assets/discord-background.png" alt="Discord Hero" />
+        </div>
+    </section>
+    </div>
+    );
+    }
+
+    export default App;
